27b6115e141242ad24dc9c40eadcfcf17254b16f
ayushanand16 <ayush.anand.cse21@itbhu.ac.in>
2022-08-07 02:57:11 +0530

    Todo List Backend Using Rest APIs

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,4 +1,4 @@
-const { ToDo, Token } = require("../models");
+const { ToDo, Token, User } = require("../models");
 
 // All the given method require token.
 // So be sure to check for it before doing any stuff
@@ -7,28 +7,216 @@ const { ToDo, Token } = require("../models");
 const getAllToDo = async (req, res) => {
   // Get the token in header.
   // Use the token to get all the ToDo's of a user
+  ToDo.find({ $or : [{createdBy: req.user.id}, {collab: req.user.id}]},
+    (err,foundTodos)=>{
+      if(!err){
+        if(foundTodos){
+          console.log(req.user.id);
+          const TodosCreated = foundTodos.filter((todo)=>todo.createdBy==req.user.id);
+          const TodosCollabed = foundTodos.filter((todo)=>todo.createdBy!=req.user.id);
+          const users = TodosCollabed.map((todo)=>todo.createdBy);
+          console.log(users);
+          User.find({_id: {$in: users}},(err,foundUsers)=>{
+            if(!err){
+              if(foundUsers)
+                res.status(200).json({
+                  "Todos Created: ": TodosCreated.map((todo)=>({
+                    id: todo.id,
+                    title: todo.title,
+                  })),
+                  "Todos Collaborated: ": TodosCollabed.map((todo)=>({
+                    id: todo.id,
+                    title: todo.title,
+                    createdBy: foundUsers.find(foundUser=>todo.createdBy==foundUser.id).username
+                  }))
+              })
+           }else{
+            console.log(err);
+           }
+
+          })
+          
+        }
+      }
+    })
 };
 
 const createToDo = async (req, res) => {
   // Check for the token and create a todo
   // or throw error correspondingly
+  const title = req.body.title;
+  const todo = new ToDo({
+      title: title,
+      createdBy: req.user.id,
+      collab: []
+  })
+  todo.save()
+  .then(res.status(200).json({
+    title: title,
+    createdBy: req.user.username
+  }))
+  .catch(err=>res.status(500).send(err));
 };
 
 const getParticularToDo = async (req, res) => {
   // Get the Todo of the logged in user with given id.
+  const TodoID = req.params.id;
+  ToDo.findById({_id: req.params.id},(err,foundTodo)=>{
+    if(!err){
+      if(foundTodo){
+        User.find({_id: {$in: foundTodo.collab}},(err,foundUsers)=>{
+          if(!err){
+            if(foundUsers)
+              res.status(200).json({
+               title: foundTodo.title,
+               createdBy: foundTodo.createdBy,
+               collaborators: foundUsers.map((user)=>user.username)
+            })
+         }else{
+          console.log(err);
+         }})}else{
+        res.status(404).send("Todo Not found");
+      }
+    }else{
+      res.status(500).send(err);
+    }
+  })
 };
 
 const editToDo = async (req, res) => {
   // Change the title of the Todo with given id, and get the new title as response.
+  const title=req.body.title;
+  ToDo.findById(req.params.id,(err,foundToDo)=>{
+    if(!err){
+      if(foundToDo){
+        console.log(foundToDo);
+        if(foundToDo.createdBy!=req.user.id){
+          return res.status(401).send("Unauthorized To Edit this Todo");
+        }else{
+          foundToDo.title = title;
+          foundToDo.save().then(res.status(200).json({
+            title : foundToDo.title,
+            createdBy : req.user.username,
+            collaborators: foundToDo.collaborator
+          }))
+        }
+      }
+    }else{
+      console.log(err);
+    }
+  })
 };
 
 const editToDoPatch = async (req, res) => {
   // Change the title of the Todo with given id, and get the new title as response
+  editToDo();
 };
 
 const deleteToDo = async (req, res) => {
   //  Delete the todo with given id
+  const ToDoId = req.params.id;
+  
+  ToDo.findByIdAndDelete(ToDoId, (err)=>{
+    if(err){
+      res.status(500).send(err);
+    }else{
+      res.status(200).send("Todo Deleted Successfully");
+    }
+  })
+
 };
+
+const addCollaborator = async (req, res) => {
+  const ToDoId = req.params.id;
+  const collaborator = req.body.collaborator;
+  console.log(collaborator);
+  User.findOne({username:collaborator},(err,userFound)=>{
+      if(!err){
+        if(userFound){
+          console.log(userFound);
+          if(userFound.id==req.user.id){
+            return res.status(401).send("Todo creater cannot be Todo collaborator");
+          }
+            ToDo.findById(ToDoId,(error,foundTodo)=>{
+              if(!error){
+                if(foundTodo){
+                  console.log(userFound.id)
+                  if(foundTodo.collab && foundTodo.collab.includes(userFound.id)){
+                    return res.status(401).send("Given user is already a collaborator");
+                  }
+                if(foundTodo.collab){
+                  foundTodo.collab.push(userFound.id);
+                }else{
+                  foundTodo.collab = [userFound.id];
+                }
+                
+                foundTodo.save().then(res.status(200).json({
+                  id: foundTodo.id,
+                  title: foundTodo.title,
+                  createdBy: req.user.id,
+                  collaboratorAdded : userFound.username
+              })).catch(err=>res.status(500).send(err));
+            }else{
+              return res.status(404).send("Todo Not Found");
+            }
+            }else{
+              res.status(500).send(error);
+            }
+            })
+        }else{
+          return res.status(404).send("User Not Found");
+        }
+      }else{
+        return res.status(500).send(err);
+      }
+        
+  })
+}
+
+const removeCollaborator = async (req,res) => {
+  const ToDoId = req.params.id;
+  const collaborator = req.body.collaborator;
+  console.log(collaborator);
+  User.findOne({username:collaborator},(err,userFound)=>{
+      if(!err){
+        if(userFound){
+          console.log(userFound);
+          if(userFound.id==req.user.id){
+            return res.status(401).send("Todo creater was not a Todo collaborator");
+          }
+            ToDo.findById(ToDoId,(error,foundTodo)=>{
+              if(!error){
+                if(foundTodo){
+                  console.log(userFound.id)
+                  if(foundTodo.collab && foundTodo.collab.includes(userFound.id)){
+                    foundTodo.collab.splice(foundTodo.collab.findIndex((user)=>{user==userFound.id}));
+                  }else{
+                    return res.status(400).send("No such collaborator");
+                  }
+                
+                foundTodo.save().then(res.status(200).json({
+                  id: foundTodo.id,
+                  title: foundTodo.title,
+                  createdBy: req.user.id,
+                  collaboratorRemoved : userFound.username
+              })).catch(err=>res.status(500).send(err));
+            }else{
+              return res.status(404).send("Todo Not Found");
+            }
+            }else{
+              res.status(500).send(error);
+            }
+            })
+        }else{
+          return res.status(404).send("User Not Found");
+        }
+      }else{
+        return res.status(500).send(err);
+      }
+        
+  })
+  
+}
 
 module.exports = {
   createToDo,
@@ -37,4 +225,6 @@ module.exports = {
   editToDoPatch,
   getAllToDo,
   getParticularToDo,
+  addCollaborator,
+  removeCollaborator
 };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
 const { UserController } = require("../controllers");
+const middlewareObj = require("../middleware/token_auth");
+const { User } = require("../models");
 
 const router = Router();
 
 router.post("/login/", UserController.login);
-router.post("/signup", UserController.signup);
-router.get("/profile", UserController.profile);
+router.post("/signup", UserController.registerFieldsAreValid,UserController.validator({
+    name: "string",
+    email:"string",
+    username:"string",
+    password: "string",
+  }),UserController.signup);
+router.get("/profile", middlewareObj.isLoggedIn,UserController.profile);
 
 module.exports = router;
