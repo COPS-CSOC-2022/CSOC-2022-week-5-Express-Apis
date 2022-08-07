11658f33aa12902d9d2f22f51979eef8741ec0f7
ayushanand16 <ayush.anand.cse21@itbhu.ac.in>
2022-08-07 12:43:49 +0530

    Improve Status Codes and restrict collaborators from editing or deleting todo

--- controllers/todo.js
+++ controllers/todo.js
@@ -16,8 +16,8 @@ const getAllToDo = async (req, res) => {
           const TodosCollabed = foundTodos.filter((todo)=>todo.createdBy!=req.user.id);
           const users = TodosCollabed.map((todo)=>todo.createdBy);
           console.log(users);
-          User.find({_id: {$in: users}},(err,foundUsers)=>{
-            if(!err){
+          User.find({_id: {$in: users}},(error,foundUsers)=>{
+            if(!error){
               if(foundUsers)
                 res.status(200).json({
                   "Todos Created: ": TodosCreated.map((todo)=>({
@@ -29,14 +29,20 @@ const getAllToDo = async (req, res) => {
                     title: todo.title,
                     createdBy: foundUsers.find(foundUser=>todo.createdBy==foundUser.id).username
                   }))
-              })
+              });else{
+                    return res.status(404).send("User Not found");
+              }
            }else{
-            console.log(err);
+            return res.status(500).send(error);
            }
 
           })
           
+        }else{
+          return res.status(404).send("Todo Not found");
         }
+      }else{
+        return res.status(500).send(err);
       }
     })
 };
@@ -50,12 +56,13 @@ const createToDo = async (req, res) => {
       createdBy: req.user.id,
       collab: []
   })
+  console.log(todo);
   todo.save()
   .then(res.status(200).json({
     title: title,
     createdBy: req.user.username
   }))
-  .catch(err=>res.status(500).send(err));
+  .catch(err=>{if(err)res.status(500).send(err)});
 };
 
 const getParticularToDo = async (req, res) => {
@@ -75,10 +82,10 @@ const getParticularToDo = async (req, res) => {
          }else{
           console.log(err);
          }})}else{
-        res.status(404).send("Todo Not found");
+        return res.status(404).send("Todo Not found");
       }
     }else{
-      res.status(500).send(err);
+     return  res.status(500).send(err);
     }
   })
 };
@@ -100,9 +107,11 @@ const editToDo = async (req, res) => {
             collaborators: foundToDo.collaborator
           }))
         }
+      }else{
+        return res.status(404).send("Todo Not Found");
       }
     }else{
-      console.log(err);
+      return res.status(500).send(err);
     }
   })
 };
@@ -115,14 +124,31 @@ const editToDoPatch = async (req, res) => {
 const deleteToDo = async (req, res) => {
   //  Delete the todo with given id
   const ToDoId = req.params.id;
-  
-  ToDo.findByIdAndDelete(ToDoId, (err)=>{
-    if(err){
-      res.status(500).send(err);
-    }else{
-      res.status(200).send("Todo Deleted Successfully");
-    }
+
+  ToDo.findById(ToDoId,(err,foundTodo)=>{
+      if(!err){
+        if(foundTodo){
+            if(foundTodo.createdBy!=req.user.id){
+              return res.status(401).send("Not authorized to delete this todo");
+            }else{
+              ToDo.findByIdAndDelete(ToDoId, (err,foundToDo)=>{
+                if(err){
+                  res.status(500).send(err);
+                }else{
+                  res.status(200).send("Todo Deleted Successfully");
+                }
+              });
+            }
+        }else{
+          return res.status(404).send("Todo Not Found");
+        }
+      }else{
+        return res.status(500).send(err);
+      }
   })
+
+  
+  
 
 };
 
@@ -135,14 +161,17 @@ const addCollaborator = async (req, res) => {
         if(userFound){
           console.log(userFound);
           if(userFound.id==req.user.id){
-            return res.status(401).send("Todo creater cannot be Todo collaborator");
+            return res.status(400).send("Todo creater cannot be Todo collaborator");
           }
             ToDo.findById(ToDoId,(error,foundTodo)=>{
               if(!error){
                 if(foundTodo){
+                  if(foundTodo.createdBy!=req.user.id){
+                    return res.status(401).send("You can't add collaborators to this todo");
+                  }
                   console.log(userFound.id)
                   if(foundTodo.collab && foundTodo.collab.includes(userFound.id)){
-                    return res.status(401).send("Given user is already a collaborator");
+                    return res.status(400).send("Given user is already a collaborator");
                   }
                 if(foundTodo.collab){
                   foundTodo.collab.push(userFound.id);
@@ -182,16 +211,19 @@ const removeCollaborator = async (req,res) => {
         if(userFound){
           console.log(userFound);
           if(userFound.id==req.user.id){
-            return res.status(401).send("Todo creater was not a Todo collaborator");
+            return res.status(400).send("Todo creater was not a Todo collaborator");
           }
             ToDo.findById(ToDoId,(error,foundTodo)=>{
               if(!error){
                 if(foundTodo){
                   console.log(userFound.id)
+                  if(foundTodo.createdBy!=req.user.id){
+                    return res.status(401).send("You can't remove collaborators to this todo");
+                  }
                   if(foundTodo.collab && foundTodo.collab.includes(userFound.id)){
                     foundTodo.collab.splice(foundTodo.collab.findIndex((user)=>{user==userFound.id}));
                   }else{
-                    return res.status(400).send("No such collaborator");
+                    return res.status(404).send("No such collaborator");
                   }
                 
                 foundTodo.save().then(res.status(200).json({
